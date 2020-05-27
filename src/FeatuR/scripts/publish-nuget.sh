c7617fb21e956a22bebef9f0a4d33c5e3e79f0fd
raulcanales <dank0ner@gmail.com>
2020-05-27 11:41:25 +0200

    Push packages to MyGet

--- src/FeatuR/scripts/publish-nuget.sh
+++ src/FeatuR/scripts/publish-nuget.sh
@@ -7,8 +7,10 @@ VERSION=$(git tag)
 cd src/FeatuR/src/FeatuR
 dotnet pack -c release /p:PackageVersion=$VERSION-preview-$TRAVIS_BUILD_NUMBER --no-restore -o .
 
+echo "Pushing package to MyGet"
+
 case "$TRAVIS_BRANCH" in
   "master")
-    dotnet nuget push *.nupkg -k $NUGET_API_KEY -s https://api.nuget.org/v3/index.json
+    dotnet nuget push *.nupkg -k $NUGET_API_KEY -Source https://www.myget.org/F/featur/api/v2/package
     ;;
 esac
