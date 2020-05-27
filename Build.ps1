ffb3561a0eb1430dc87cf7d39902ee480c64e840
raulcanales <dank0ner@gmail.com>
2020-05-27 22:13:50 +0200

    Added Appveyor

---
+++ Build.ps1
@@ -0,0 +1,49 @@
+# Taken from MediatR https://github.com/jbogard/MediatR
+
+<#
+.SYNOPSIS
+  This is a helper function that runs a scriptblock and checks the PS variable $lastexitcode
+  to see if an error occcured. If an error is detected then an exception is thrown.
+  This function allows you to run command-line programs without having to
+  explicitly check the $lastexitcode variable.
+.EXAMPLE
+  exec { svn info $repository_trunk } "Error executing SVN. Please verify SVN command-line client is installed"
+#>
+function Exec
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0,Mandatory=1)][scriptblock]$cmd,
+        [Parameter(Position=1,Mandatory=0)][string]$errorMessage = ($msgs.error_bad_command -f $cmd)
+    )
+    & $cmd
+    if ($lastexitcode -ne 0) {
+        throw ("Exec: " + $errorMessage)
+    }
+}
+
+if(Test-Path .\src\FeatuR\src\FeatuR\artifacts) { Remove-Item .\src\FeatuR\src\FeatuR\artifacts -Force -Recurse }
+
+$branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
+$revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+$suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)))-$revision"}[$branch -eq "master" -and $revision -ne "local"]
+$commitHash = $(git rev-parse --short HEAD)
+$buildSuffix = @{ $true = "$($suffix)-$($commitHash)"; $false = "$($branch)-$($commitHash)" }[$suffix -ne ""]
+$versionSuffix = @{ $true = "--version-suffix=$($suffix)"; $false = ""}[$suffix -ne ""]
+
+echo "build: Package version suffix is $suffix"
+echo "build: Build version suffix is $buildSuffix" 
+
+exec { & dotnet build FeatuR.sln -c Release --version-suffix=$buildSuffix }
+
+Push-Location -Path .\test\FeatuR.Tests
+
+try {
+    exec { & dotnet test -c Release --no-build --no-restore }
+} finally {
+    Pop-Location
+}
+
+Pop-Location
+
+exec { & dotnet pack .\src\FeatuR\src\FeatuR.csproj -c Release -o .\artifacts --include-symbols --no-build $versionSuffix }
