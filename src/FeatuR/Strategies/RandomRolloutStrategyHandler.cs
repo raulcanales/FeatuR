c69d375ff7cf766bdde25de2be06f07150eec745
thomjn <thomasjneergaard@gmail.com>
2020-06-09 22:27:11 +0200

    WIP

---
+++ src/FeatuR/Strategies/RandomRolloutStrategyHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FeatuR.Strategies
+{
+    /// <summary>
+    /// Enables a given feature for random users based on specified percentage.
+    /// </summary>
+    public class RandomRolloutStrategyHandler : IStrategyHandler
+    {
+        public bool IsEnabled(Dictionary<string, string> parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEnabled(Dictionary<string, string> parameters, IFeatureContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsEnabledAsync(Dictionary<string, string> parameters, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsEnabledAsync(Dictionary<string, string> parameters, IFeatureContext context, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
