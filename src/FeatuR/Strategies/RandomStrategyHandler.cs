8cf17e924a192c75e9fbe96e0cefc9fdcb5b9261
thomjn <thomasjneergaard@gmail.com>
2020-06-10 20:54:04 +0200

    Naive random feature enabler.

--- src/FeatuR/Strategies/RandomStrategyHandler.cs
+++ src/FeatuR/Strategies/RandomStrategyHandler.cs
@@ -6,28 +6,20 @@ using System.Threading.Tasks;
 namespace FeatuR.Strategies
 {
     /// <summary>
-    /// Enables a given feature for random users based on specified percentage.
+    /// Enables a given feature for random users based on a simple/naive randomizer.
     /// </summary>
-    public class RandomRolloutStrategyHandler : IStrategyHandler
+    public class RandomStrategyHandler : IStrategyHandler
     {
         public bool IsEnabled(Dictionary<string, string> parameters)
-        {
-            throw new NotImplementedException();
-        }
+            => IsEnabled(parameters, null);
 
         public bool IsEnabled(Dictionary<string, string> parameters, IFeatureContext context)
-        {
-            throw new NotImplementedException();
-        }
+            => new Random().Next(1, 100) % 2 == 0;
 
         public Task<bool> IsEnabledAsync(Dictionary<string, string> parameters, CancellationToken token)
-        {
-            throw new NotImplementedException();
-        }
+            => Task.FromResult(IsEnabled(parameters, null));
 
         public Task<bool> IsEnabledAsync(Dictionary<string, string> parameters, IFeatureContext context, CancellationToken token)
-        {
-            throw new NotImplementedException();
-        }
+            => Task.FromResult(IsEnabled(parameters, context));
     }
 }
---
+++ test/FeatuR.Tests/StrategyTests/RandomStrategyHandlerTest.cs
@@ -0,0 +1,39 @@
+﻿using FeatuR.Strategies;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FeatuR.Tests.StrategyTests
+{
+    public class RandomStrategyHandlerTest
+    {
+        private readonly RandomStrategyHandler _sut;
+        private readonly IFeatureContext _context;
+        private readonly Dictionary<string, string> _activationStrategies;
+
+        public RandomStrategyHandlerTest()
+        {
+            _sut = new RandomStrategyHandler();
+            _context = new FeatureContext();
+            _activationStrategies = new Dictionary<string, string>();
+        }
+
+        [Fact]
+        public async Task IsEnabled_ShouldBeRandomlyEnabled()
+        {
+            var results = new List<bool>();
+            _context.Parameters.Add("", "");
+            _activationStrategies.Add("", "");
+
+            for (int i = 0; i < 100; i++)
+            {
+                results.Add(_sut.IsEnabled(_activationStrategies, _context));
+                results.Add(await _sut.IsEnabledAsync(_activationStrategies, _context, CancellationToken.None));
+            }
+
+            Assert.Contains(true, results);
+            Assert.Contains(false, results);
+        }
+    }
+}
