90a9b241874d12ba3a4261138cfb959a125073f1
ThomJN <thomasjneergaard@gmail.com>
2020-06-24 17:24:05 +0200

    Randomstrategyhandler (#8)
    * minor stuff
    
    * WIP
    
    * Naive random feature enabler.
    
    * PR feedback. Renamed dictionary. Moved Random initialization into constructor.

---
+++ src/FeatuR/Strategies/RandomStrategyHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FeatuR.Strategies
+{
+    /// <summary>
+    /// Enables a given feature for random users based on a simple/naive randomizer.
+    /// </summary>
+    public class RandomStrategyHandler : IStrategyHandler
+    {
+        private readonly Random _random;
+
+        public RandomStrategyHandler()
+        {
+            _random = new Random();
+        }
+
+        public bool IsEnabled(Dictionary<string, string> parameters)
+            => IsEnabled(parameters, null);
+
+        public bool IsEnabled(Dictionary<string, string> parameters, IFeatureContext context)
+            => _random.Next(1, 100) % 2 == 0;
+
+        public Task<bool> IsEnabledAsync(Dictionary<string, string> parameters, CancellationToken token)
+            => Task.FromResult(IsEnabled(parameters, null));
+
+        public Task<bool> IsEnabledAsync(Dictionary<string, string> parameters, IFeatureContext context, CancellationToken token)
+            => Task.FromResult(IsEnabled(parameters, context));
+    }
+}
---
+++ test/FeatuR.Tests/StrategyTests/RandomStrategyHandlerTest.cs
@@ -0,0 +1,37 @@
+﻿using FeatuR.Strategies;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FeatuR.Tests.StrategyTests
+{
+    public class RandomStrategyHandlerTest
+    {
+        private readonly RandomStrategyHandler _sut;
+        private readonly IFeatureContext _context;
+        private readonly Dictionary<string, string> _activationParameters;
+
+        public RandomStrategyHandlerTest()
+        {
+            _sut = new RandomStrategyHandler();
+            _context = new FeatureContext();
+            _activationParameters = new Dictionary<string, string>();
+        }
+
+        [Fact]
+        public async Task IsEnabled_ShouldBeRandomlyEnabled()
+        {
+            var results = new List<bool>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                results.Add(_sut.IsEnabled(_activationParameters, _context));
+                results.Add(await _sut.IsEnabledAsync(_activationParameters, _context, CancellationToken.None));
+            }
+
+            Assert.Contains(true, results);
+            Assert.Contains(false, results);
+        }
+    }
+}
